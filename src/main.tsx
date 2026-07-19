421e17997aa4e66c4ea9eff574e1bbcc6e20bbcc
iamasher0912 <georgelagares@gmail.com>
2026-07-19 11:03:57 -0400

    Refactor main.tsx to use StrictMode and App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <div style={{
-    width: '100vw',
-    height: '100vh',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    background: '#13161f',
-    color: 'white',
-    fontSize: '32px',
-    fontFamily: 'Arial'
-  }}>
-    FINLY TEST OK
-  </div>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
